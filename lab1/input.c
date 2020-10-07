ed0bbe6c33690fca08ad3c82abb30e33c6b1d96d
wh75er <wh75er@gmail.com>
2020-10-07 15:33:00 +0300

    Удаление хардкода stdin и добавление функций для чтения чисел и строк из потока
    Изменение описания функций
    
    change get_array function name

--- lab1/input.c
+++ lab1/input.c
@@ -4,14 +4,14 @@
  * Получение пользовательских данных
  */
 int
-get_user_input(int* size, struct Class* arr) {
+get_user_input(FILE* stream, int* size, struct Class* arr) {
   int err = 0;
-  if ((err = get_size(size))) {
+  if ((err = get_value_from_stream(stream, size))) {
     fprintf(stderr, "Возникла ошибка при получении размера массива!\n");
     return err;
   }
 
-  if ((err = get_array(size, arr))) {
+  if ((err = get_array_from_stream(stream, size, arr))) {
     fprintf(stderr, "Возникла ошибка при чтении расписания!\n");
     return err;
   }
@@ -20,30 +20,34 @@ get_user_input(int* size, struct Class* arr) {
 }
 
 /*
- * Считывание размера массива
+ * Считывание целочисленных значений из потока
  */
 int
-get_size(int* size) {
-  bool isNumber = false;
-  while (isNumber == false) {
+get_value_from_stream(FILE* stream, int* val) {
+  bool is_number = false;
+  while (is_number == false) {
     printf("Пожалуйста, введите размер массива: ");
 
     char buffer[BUFFER_SIZE];
-    if (!fgets(buffer, BUFFER_SIZE, stdin)) {
-      fprintf(stderr, "Возникла ошибка при заполнения буфера из stdin!\n");
+    if (!fgets(buffer, BUFFER_SIZE, stream)) {
+      fprintf(stderr, "Возникла ошибка при заполнения буфера из потока!\n");
       return errno;
     }
 
-    int n = sscanf(" %d", size);
-    
-    fflush_stdin();
+    int n = sscanf(buffer, " %d", val);
 
     if (n == 1) {
-      isNumber = true;
+      is_number = true;
     } else if (!errno)  {
       printf("Введенное вами значение не является числом. Повторите...\n");
     } else {
-      fprintf(stderr, "Возникла ошибка при считывании значения");
+      fprintf(stderr, "Возникла ошибка при считывании значения!\n");
+      return errno;
+    }
+
+    fflush_stream();
+    if (errno) {
+      fprintf(stderr, "Возникла ошибка при чистке буфера потока!\n");
       return errno;
     }
   }
@@ -51,14 +55,50 @@ get_size(int* size) {
   return 0;
 }
 
-int   get_array(int* size, struct Class* arr);
+/*
+ * Считывание строк из потока
+ */
+int
+get_string_from_stream(FILE* stream, char* str) {
+  bool is_number = false;
+  while (is_number == false) {
+    printf("Пожалуйста, введите размер массива: ");
+
+    char buffer[BUFFER_SIZE];
+    if (!fgets(buffer, BUFFER_SIZE, stream)) {
+      fprintf(stderr, "Возникла ошибка при заполнения буфера из потока!\n");
+      return errno;
+    }
+
+    int n = sscanf(buffer, " %ms", str);
+
+    if (n == 1) {
+      is_number = true;
+    } else if (!errno)  {
+      printf("Введенное вами значение не является строкой. Повторите...\n");
+    } else {
+      fprintf(stderr, "Возникла ошибка при считывании значения!\n");
+      return errno;
+    }
+
+    fflush_stream();
+    if (errno) {
+      fprintf(stderr, "Возникла ошибка при чистке буфера потока!\n");
+      return errno;
+    }
+  }
+
+  return 0;
+}
+
+int   get_array_from_stream(FILE* stream, int* size, struct Class* arr);
 
 /*
- * Чистит поток stdin до начала новой строки
+ * Чистит поток до начала новой строки
  * или до конца потока
  */
 void
-fflush_stdin(void) {
+fflush_stream(FILE* stream) {
   int c;
-  while ((c = fgetc(stdin)) != EOF && c != '\n');
+  while ((c = fgetc(stream)) != EOF && c != '\n');
 }
--- lab1/input.h
+++ lab1/input.h
@@ -9,9 +9,10 @@
 
 #define BUFFER_SIZE 10
 
-int   get_user_input(int *, struct Class *);
-int   get_size(int *);
-int   get_array(int *, struct Class *);
-void  fflush_stdin(void);
+int   get_user_input(FILE *, int *, struct Class *);
+int   get_value_from_stream(FILE *, int *);
+int   get_string_from_stream(FILE *, char *);
+int   get_array_from_stream(FILE *, int *, struct Class *);
+void  fflush_stream(FILE *);
 
 #endif
