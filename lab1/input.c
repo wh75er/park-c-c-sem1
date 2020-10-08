dada3d0299c2b13fed496e0099cf6c8f325581cd
wh75er <wh75er@gmail.com>
2020-10-08 13:13:44 +0300

    Добавление функций для считывания полей структуры

--- lab1/input.c
+++ lab1/input.c
@@ -5,6 +5,7 @@
  */
 int
 get_user_input(FILE* stream, int* size, struct Class* arr) {
+  printf("Пожалуйста, введите размер массива: ");
   int err = 0;
   if ((err = get_value_from_stream(stream, size))) {
     fprintf(stderr, "Возникла ошибка при получении размера массива!\n");
@@ -20,14 +21,12 @@ get_user_input(FILE* stream, int* size, struct Class* arr) {
 }
 
 /*
- * Считывание целочисленных значений из потока
+ * Считывание целочисленного значения из потока
  */
 int
 get_value_from_stream(FILE* stream, int* val) {
   bool is_number = false;
   while (is_number == false) {
-    printf("Пожалуйста, введите размер массива: ");
-
     char buffer[BUFFER_SIZE];
     if (!fgets(buffer, BUFFER_SIZE, stream)) {
       fprintf(stderr, "Возникла ошибка при заполнения буфера из потока!\n");
@@ -56,14 +55,12 @@ get_value_from_stream(FILE* stream, int* val) {
 }
 
 /*
- * Считывание строк из потока
+ * Считывание строки из потока
  */
 int
 get_string_from_stream(FILE* stream, char* str) {
   bool is_number = false;
   while (is_number == false) {
-    printf("Пожалуйста, введите размер массива: ");
-
     char buffer[BUFFER_SIZE];
     if (!fgets(buffer, BUFFER_SIZE, stream)) {
       fprintf(stderr, "Возникла ошибка при заполнения буфера из потока!\n");
@@ -78,12 +75,16 @@ get_string_from_stream(FILE* stream, char* str) {
       printf("Введенное вами значение не является строкой. Повторите...\n");
     } else {
       fprintf(stderr, "Возникла ошибка при считывании значения!\n");
+      free(str);
+      str = NULL;
       return errno;
     }
 
     fflush_stream();
     if (errno) {
       fprintf(stderr, "Возникла ошибка при чистке буфера потока!\n");
+      free(str);
+      str = NULL;
       return errno;
     }
   }
@@ -91,7 +92,229 @@ get_string_from_stream(FILE* stream, char* str) {
   return 0;
 }
 
-int   get_array_from_stream(FILE* stream, int* size, struct Class* arr);
+/*
+ * Считывание дня недели из потока
+ */
+int
+get_weekday_from_stream(FILE* stream, int* weekday) {
+  printf("Введите день недели[0-ПН, 6-ВС]: ");
+  *weekday = -1;
+  for (;;) {
+    if ((int err = get_value_from_stream(stream, &weekday))) {
+      return err;
+    }
+
+    if (*weekday < 0 || *weekday > 6) {
+      printf("Ваше значение не является днем недели. Пожалуйста, введите корректное число[0-ПН, 6-ВС]: ");
+    } else {
+      break;
+    }
+  }
+
+  return 0;
+}
+
+/*
+ * Считывание времени из потока
+ */
+int
+get_time_from_stream(FILE* stream, int* time) {
+  printf("Введите время начала занятия в минутах[8:30 - 510, 20:30 - 1230]: ");
+  *time = -1;
+  for (;;) {
+    if ((int err = get_value_from_stream(stream, &time))) {
+      return err;
+    }
+
+    if (*time < 510 || *time > 1230) {
+      printf("Ваше значение не удовлетворяет условию. Пожалуйста, введите корректное число[510, 1230]: ");
+    } else {
+      break;
+    }
+  }
+
+  return 0;
+}
+
+/*
+ * Считывании информации о типе занятия
+ */
+int
+get_lecture_from_stream(FILE* stream, bool* lecture) {
+  printf("Введите тип занятия[0 - семинар, 1 - лекция]: ");
+  int type = -1;
+  for (;;) {
+    if ((int err = get_value_from_stream(stream, &type))) {
+      return err;
+    }
+
+    if (type < 0 || type > 1) {
+      printf("Ваше значение не удовлетворяет условию. Пожалуйста, введите корректное число[0 - семинар или 1 - лекция]: ");
+    } else {
+      *lecture = type;
+      break;
+    }
+  }
+
+  return 0;
+}
+
+/*
+ * Заполнение названия предмета
+ */
+int
+get_subject_from_stream(FILE* stream, char* subject) {
+  printf("Введите название предмета: ");
+  char* subject;
+  if ((int err = get_string_from_stream(stream, &subject))) {
+    return err;
+  }
+
+  return 0;
+}
+
+/*
+ * Заполнение имени преподавателя
+ */
+int
+get_professor_from_stream(FILE* stream, char* professor) {
+  printf("Введите имя преподавателя: ");
+  char* professor;
+  if ((int err = get_string_from_stream(stream, &professor))) {
+    return err;
+  }
+
+  return 0;
+}
+
+/*
+ * Заполнение курса
+ */
+int
+get_year_from_stream(FILE* stream, int* year) {
+  printf("Введите курс: ");
+  *year = -1;
+  for (;;) {
+    if ((int err = get_value_from_stream(stream, &year))) {
+      return err;
+    }
+
+    if (*year < 1 || *year > 10) {
+      printf("Ваше значение не удовлетворяет условию. Пожалуйста, введите корректное число[1, 10]: ");
+    } else {
+      break;
+    }
+  }
+
+  return 0;
+}
+
+/*
+ * Заполнение номера группы
+ */
+int
+get_group_from_stream(FILE* stream, int* group) {
+  printf("Введите номер группы: ");
+  *group = -1;
+  for (;;) {
+    if ((int err = get_value_from_stream(stream, &group))) {
+      return err;
+    }
+
+    if (*group < 1 || *group > 100) {
+      printf("Ваше значение не удовлетворяет условию. Пожалуйста, введите корректное число[1, 100]: ");
+    } else {
+      break;
+    }
+  }
+
+  return 0;
+}
+
+/*
+ * Заполнение одного объекта из потока по указателю
+ */
+int
+get_object_from_stream(FILE* stream, struct Class* obj) {
+  int err;
+  int weekday = -1;
+  if ((err = get_weekday_from_stream(stream, &weekday))) {
+    fprintf(stderr, "Возникла ошибка при чтении дня недели!\n");
+    return err;
+  }
+  (*obj).weekday = weekday;
+
+  int time;
+  if ((err = get_time_from_stream(stream, &time))) {
+    fprintf(stderr, "Возникла ошибка при чтении времени начала занятия!\n");
+    return err;
+  }
+  (*obj).time = time;
+
+  bool lecture = 0;
+  if ((err = get_lecture_from_stream(stream, &lecture))) {
+    fprintf(stderr, "Возникла ошибка при чтении типа занятия!\n");
+    return err;
+  }
+  (*obj).lecture = lecture;
+
+  int duration = -1;
+  if ((err = get_duration_from_stream(stream, &duration))) {
+    fprintf(stderr, "Возникла ошибка при чтении времени продолжительности занятия!\n");
+    return err;
+  }
+  (*obj).duration = duration;
+
+  char* subject = NULL;
+  if ((err = get_subject_from_stream(stream, subject))) {
+    fprintf(stderr, "Возникла ошибка при чтении названия занятия!\n");
+    return err;
+  }
+  (*obj).subject = subject;
+
+  char* professor = NULL;
+  if ((err = get_subject_from_stream(stream, professor))) {
+    fprintf(stderr, "Возникла ошибка при чтении имени преподавателя!\n");
+    return err;
+  }
+  (*obj).professor = professor;
+
+  int year = -1;
+  if ((err = get_subject_from_stream(stream, &year))) {
+    fprintf(stderr, "Возникла ошибка при чтении номера курса!\n");
+    return err;
+  }
+  (*obj).year = year;
+
+  int group = -1;
+  if ((err = get_subject_from_stream(stream, &group))) {
+    fprintf(stderr, "Возникла ошибка при чтении номера группы!\n");
+    return err;
+  }
+  (*obj).group = group;
+
+  return 0;
+}
+
+/*
+ * Выделение памяти под массив и его заполнение из потока
+ */
+int
+get_array_from_stream(FILE* stream, int size, struct Class* arr) {
+  int err;
+  if ((err = arralloc(size, arr))) {
+    fprintf(stderr, "Возникла ошибка при выделении памяти под массив!\n");
+    return err;
+  }
+
+  for (int i = 0; i < size; i++) {
+    if((err = get_object_from_stream(stream, &arr[i]))) {
+      fprintf(stderr, "Возникла ошибка при заполнении массива!\n");
+      return err;
+    }
+  }
+
+}
 
 /*
  * Чистит поток до начала новой строки
--- lab1/input.h
+++ lab1/input.h
@@ -10,9 +10,23 @@
 #define BUFFER_SIZE 10
 
 int   get_user_input(FILE *, int *, struct Class *);
+
 int   get_value_from_stream(FILE *, int *);
 int   get_string_from_stream(FILE *, char *);
-int   get_array_from_stream(FILE *, int *, struct Class *);
+
+int   get_object_from_stream(FILE *, struct Class *);
+
+int   get_weekday_from_stream(FILE *, int *);
+int   get_time_from_stream(FILE *, int *);
+int   get_lecture_from_stream(FILE *, bool *);
+int   get_duration_from_stream(FILE *, int *);
+int   get_subject_from_stream(FILE *, char *);
+int   get_professor_from_stream(FILE *, char *);
+int   get_year_from_stream(FILE *, int *);
+int   get_group_from_stream(FILE *, int *);
+
+int   get_array_from_stream(FILE *, int, struct Class *);
+
 void  fflush_stream(FILE *);
 
 #endif
