f1910bb37aebea2d3ebe2167b84ea4d90efa2841
wh75er <wh75er@gmail.com>
2020-10-10 12:33:31 +0300

    Добавление комментария к функции обработки ошибок и вывод в stderr

--- lab1/src/errors.c
+++ lab1/src/errors.c
@@ -1,11 +1,14 @@
 #include "errors.h"
 
+/*
+ * Выводит сообщение об ошибке и высвобождает память
+ */
 void
 error_handler(const int err, const int arr_size, struct class** arr, const int path_size, struct class** path) {
   if (err > 0) {
-    printf("Error occured: %s!\n", strerror(err));
+    fprintf(stderr, "Error occured: %s!\n", strerror(err));
   } else {
-    printf("Error occured!\n");
+    fprintf(stderr, "Error occured!\n");
   }
 
   arrfree(arr_size, arr);
