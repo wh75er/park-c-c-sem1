c51376e7d554b1e01c150e50e33ba4eb845cfc5f
wh75er <wh75er@gmail.com>
2020-10-11 15:44:20 +0300

    Добавляет функционал тестирования с пробными тестами

--- lab1/tests/testlib.cpp
+++ lab1/tests/testlib.cpp
@@ -1,14 +1,44 @@
 #include <gtest/gtest.h>
-#include <iostream>
+#include <fstream>
+#include <stdio.h>
 
-#include "input.h"
+extern "C" {
+  #include "input.h"
+}
+
+#define TEST_DATA_FILENAME "test.data"
 
 TEST(TestInputStream, WeekdayInput) {
-  std::stringstream stream;
-  stream << "2\n";
+  std::ofstream file(TEST_DATA_FILENAME);
+  file << "2\n";
+  file.close();
+
+  FILE* stream = fopen(TEST_DATA_FILENAME, "r");
+  ASSERT_TRUE(stream);
   
   int weekday = 0;
   int err = get_weekday_from_stream(stream, &weekday);
+
+  fclose(stream);
   
+  EXPECT_EQ(err, 0);
+  EXPECT_EQ(weekday, 2);
+}
+
+TEST(TestInputStream, WeekdayInputValidation) {
+  std::ofstream file(TEST_DATA_FILENAME);
+  file << "-1\n";
+  file << "2\n";
+  file.close();
+
+  FILE* stream = fopen(TEST_DATA_FILENAME, "r");
+  ASSERT_TRUE(stream);
+  
+  int weekday = 0;
+  int err = get_weekday_from_stream(stream, &weekday);
+
+  fclose(stream);
+  
+  EXPECT_EQ(err, 0);
   EXPECT_EQ(weekday, 2);
 }
