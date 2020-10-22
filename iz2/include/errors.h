d3d67f60fb866f5418779399c21e5b5e559b0852
wh75er <wh75er@gmail.com>
2020-10-22 18:29:18 +0300

    Add errors enum

--- iz2/include/errors.h
+++ iz2/include/errors.h
@@ -1,16 +1,10 @@
 #ifndef __ERRORS_H__
 #define __ERRORS_H__
 
-#include <stdio.h>
-#include <error.h>
-
-#include "pos.h"
-
 enum custom_errors {
   TYPE_ERR = -2,
-  COPY_ERR = -3
+  COPY_ERR = -3,
+  ARGS_ERR = -4,
 };
-
-void  error_handler(const int err, struct pos** arr);
 
 #endif
