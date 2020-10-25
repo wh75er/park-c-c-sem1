abf6c76f42d1bd82b0848bd01e8d1cbe8ef1702b
wh75er <wh75er@gmail.com>
2020-10-25 18:47:55 +0300

    Add parallel implementation

--- iz2/src/parallel_mean.c
+++ iz2/src/parallel_mean.c
@@ -1,12 +1,29 @@
-#include <errno>
+#include <fcntl.h>
+#include <semaphore.h>
 #include <stdlib.h>
+#include <sys/mman.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/sysinfo.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 #include "mean.h"
 
-int get_sum(const struct pos * const arr, const size_t left_border, const size_t right_border, struct pos * const sum) {
+#define SEM_MUTEX_NAME "/sum-mutex"
+
+int get_processes_number(const size_t size) {
+  int n = get_nprocs();
+
+  if (n && !(size / n)) {
+    n = size;
+  }
+
+  return n;
+}
+
+int
+get_sum(const struct pos * const arr, const size_t left_border, const size_t right_border, struct pos * const sum) {
   if(left_border > right_border) {
     return SUM_BORDERS_ERR;
   }
@@ -30,9 +47,117 @@ int get_sum(const struct pos * const arr, const size_t left_border, const size_t
 
 int
 find_mean(const struct pos * const arr, const size_t size, struct pos * const mean) {
-  int fd[2];
-
-  if(pipe(fd)) {
-    return PARALLEL_PIPE_ERR;
+  if(!arr || !size || !mean) {
+    return PARALLEL_PARAMS_ERR;
   }
+
+  int n = 0;
+  if(!(n = get_processes_number(size))) {
+    return PARALLEL_PROC_BUSY_ERR;
+  }
+  printf("Number of processes: %d\n", n);
+
+  struct pos* shared_sum = mmap(NULL, sizeof(struct pos), 
+                                PROT_READ | PROT_WRITE,
+                                MAP_SHARED | MAP_ANONYMOUS,
+                                -1, 0);
+
+  if(!shared_sum) {
+    return PARALLEL_SHMEM_MAP_ERR;
+  }
+
+  sem_t* mutex_sem = SEM_FAILED;
+  if ((mutex_sem = sem_open (SEM_MUTEX_NAME, O_CREAT, ACCESSPERMS, 1)) == SEM_FAILED) {
+      munmap(shared_sum, sizeof(struct pos));
+      return PARALLEL_SEM_OPEN_ERR;
+    }
+
+  pid_t nchildren[n];
+  for(size_t child_idx = 0; child_idx < n; child_idx++) {
+    if((nchildren[child_idx] = fork()) < 0) {
+      munmap(shared_sum, sizeof(struct pos));
+      sem_close(mutex_sem);
+      unlink(SEM_MUTEX_NAME);
+
+      return PARALLEL_CHILD_CREATE_ERR;
+    } else if(!nchildren[child_idx]) {
+      size_t offset = size / n;
+
+      size_t left_border = offset * child_idx;
+      size_t right_border = left_border + offset;
+
+      printf("Child(%d) left border: %d\n", nchildren[child_idx], left_border);
+      printf("Child(%d) right border: %d\n", nchildren[child_idx], right_border);
+      printf("Child(%d) offset: %d\n", nchildren[child_idx], offset);
+      printf("\n");
+
+      if(right_border > size || child_idx + 1 == n) {
+        right_border = size;
+      }
+
+      struct pos sum = {0, 0, 0};
+      int err = SUCCESS;
+      if((err = get_sum(arr, left_border, right_border, &sum))) {
+        _exit(err);
+      }
+
+      sem_wait(mutex_sem);
+
+      shared_sum->x += sum.x;
+      shared_sum->y += sum.y;
+      shared_sum->z += sum.z;
+
+      if(sem_post(mutex_sem)) {
+        _exit(PARALLEL_SEM_POST_ERR);
+      };
+
+      _exit(EXIT_SUCCESS);
+    }
+  }
+
+  size_t count = 0; 
+  int status = 0;
+  while(count < n) {
+    pid_t wpid = waitpid(-1, &status, 0);
+
+    if(wpid < 0) {
+      munmap(shared_sum, sizeof(struct pos));
+      sem_close(mutex_sem);
+      unlink(SEM_MUTEX_NAME);
+
+      return PARALLEL_PWAIT_ERR;
+    }
+
+    if (WIFEXITED(status)) {
+      int exit_status = WEXITSTATUS(status);
+
+      if(exit_status) {
+        munmap(shared_sum, sizeof(struct pos));
+        sem_close(mutex_sem);
+        unlink(SEM_MUTEX_NAME);
+
+        return exit_status;
+      }
+
+      count++;
+    } else {
+      munmap(shared_sum, sizeof(struct pos));
+      sem_close(mutex_sem);
+      unlink(SEM_MUTEX_NAME);
+
+      return PARALLEL_CHILD_FAILURE_ERR;
+    }    
+  }
+
+  print(stdout, shared_sum);
+
+  mean->x = shared_sum->x / size;
+  mean->y = shared_sum->y / size;
+  mean->z = shared_sum->z / size;
+
+  munmap(shared_sum, sizeof(struct pos));
+  sem_close(mutex_sem);
+  unlink(SEM_MUTEX_NAME);
+
+  return SUCCESS;
 }
